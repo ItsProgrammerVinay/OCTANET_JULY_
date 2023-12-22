685588edf2e02d282b50cdfd5f2ef8c9c7eacb0e
ItsProgrammerVinay <119567736+ItsProgrammerVinay@users.noreply.github.com>
2023-12-22 11:40:05 +0000

    min array and reapeting problems solved

--- Array_target_sum/Questions.java
+++ Array_target_sum/Questions.java
@@ -1,11 +1,47 @@
 class Questions{
     public static void main(String[] args) {
         
-        int arr[]={2,4,3,5,6,44};
+        int arr[]={1,4,2,3,4,5,2};
 
-        System.out.println("Max Element in arr is:"+maxElement(arr));
-        System.out.println("Second Element in arr is:"+secMaxElement(arr));
+        // System.out.println("Max Element in arr is:"+maxElement(arr));
+        // System.out.println("Second Element in arr is:"+secMaxElement(arr));
+        // System.out.println("MIN Element in arr is:"+minElement(arr));
+        // System.out.println("Sec Min Element in arr is:"+secMinElement(arr));
+        System.out.println("first Reapeating Number is :"+firstReapeatingNumber(arr));
     }
+
+    public static int firstReapeatingNumber(int arr[]){
+        int n=arr.length;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;i++){
+                if(arr[i]==arr[j]){
+                    return arr[i];
+                }
+            }
+        }
+        return -1;
+    }
+    public static int minElement(int arr[]){
+        int min_value=Integer.MAX_VALUE;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]<min_value){
+                min_value=arr[i];
+            }
+        }
+        return min_value;
+    }
+
+    public static int secMinElement(int arr[]){
+        int min=minElement(arr);
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==min){
+                arr[i]=Integer.MAX_VALUE;
+            }
+        }
+        int sec_min=minElement(arr);
+        return sec_min;
+    }
+
     public static int maxElement(int arr[]){
         int max_value=Integer.MIN_VALUE;
         for(int i=0;i<arr.length;i++){
