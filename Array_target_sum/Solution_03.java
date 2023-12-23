f341507c16c76fbb27948c3f8f6f793871682bab
ItsProgrammerVinay <119567736+ItsProgrammerVinay@users.noreply.github.com>
2023-12-23 12:44:49 +0000

    sort Square Array

--- Array_target_sum/Solution_03.java
+++ Array_target_sum/Solution_03.java
@@ -1,14 +1,37 @@
 public class Solution_03 {
   public static void main(String[] args) {
-    int arr[] = { 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0 };
-    sortZerosAnsOnce(arr);
-    printArrays(arr);
+    int arr[] = { 1,2,3,4,5,6 };
+    // sortZerosAnsOnce(arr);
+    int ans[] = sortSquares(arr);
+
+    printArrays(ans);
   }
+
   public static void printArrays(int arr[]) {
     int n = arr.length;
     for (int i = 0; i < n; i++) {
       System.out.print(arr[i] + " ");
     }
+  }
+
+  public static int[] sortSquares(int arr[]) {
+    int n = arr.length;
+    int ans[] = new int[n];
+    int k=n-1;
+    int left = 0;
+    int right=n-1;
+    while (left <= right) {
+      if (Math.abs(arr[left]) > Math.abs(arr[right])) {
+        ans[k--] = arr[left] * arr[left];
+        left++;
+
+      } else {
+        ans[k--] = arr[right] * arr[right];
+        right--;
+      }
+
+    }
+    return ans;
   }
   public static void sortZerosAnsOnce(int arr[]){
     int n=arr.length;
